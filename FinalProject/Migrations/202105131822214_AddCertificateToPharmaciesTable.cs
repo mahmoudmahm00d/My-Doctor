6a988f6e1c35f6522a8e04cbc1500e7e4e8db152
mahmoud mahmoud <mahmoud.mahmoud.2000@outlook.com>
2021-05-13 21:24:05 +0300

    Add Certificate Migration

---
+++ FinalProject/Migrations/202105131822214_AddCertificateToPharmaciesTable.cs
@@ -0,0 +1,18 @@
+﻿namespace FinalProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCertificateToPharmaciesTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Pharmacies", "Certificate", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Pharmacies", "Certificate");
+        }
+    }
+}
